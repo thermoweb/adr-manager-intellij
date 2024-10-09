3df5354bd5c024f81572c00e5da728239f7617fb
romain.chauveau <romain.chauveau@praxedo.com>
2024-10-09 16:56:26 +0200

    make service retrieve actual ADRs

--- src/main/java/com/github/thermoweb/adr/AdrService.java
+++ src/main/java/com/github/thermoweb/adr/AdrService.java
@@ -1,13 +1,43 @@
 package com.github.thermoweb.adr;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import com.intellij.openapi.components.Service;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 
 @Service(Service.Level.PROJECT)
 public final class AdrService {
+    public static final Logger log = Logger.getInstance(AdrService.class);
+
+    public final Project project;
+
+    public AdrService(Project project) {
+        this.project = project;
+    }
 
     public List<Adr> getAdrs() {
-        return List.of(new Adr("20241009", "my first fake adr !"), new Adr("20240101", "my older adr"));
+        return listFilesIn(project).stream()
+                .map(adrFile -> new Adr(adrFile.getName(), adrFile.getName()))
+                .toList();
+    }
+
+    private List<VirtualFile> listFilesIn(Project project) {
+        if (project == null || project.getBasePath() == null) {
+            log.error("Project path is null");
+            return Collections.emptyList();
+        }
+        VirtualFile projectDir = LocalFileSystem.getInstance().findFileByPath(project.getBasePath() + "/docs/adr");
+        if (projectDir != null && projectDir.isDirectory()) {
+            return Arrays.stream(projectDir.getChildren())
+                    .filter(f -> f.getName().endsWith(".md"))
+                    .filter(f -> !f.isDirectory())
+                    .toList();
+        }
+        return Collections.emptyList();
     }
 }
